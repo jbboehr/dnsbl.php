29b11430bfd99a66f94b01dfb75699611018f0ee
John Boehr <john@johnboehr.is>
2025-10-22 08:50:31 -0700

    Add back PHP 7.4 to CI

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "jbboehr/dnsbl.php";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils = {
       url = "github:numtide/flake-utils";
@@ -15,69 +15,80 @@
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-phps.url = "github:fossar/nix-phps";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    flake-utils,
-    pre-commit-hooks,
-    gitignore,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      buildEnv = php:
-        php.buildEnv {
-          extraConfig = "memory_limit = 2G";
-          extensions = {
-            enabled,
-            all,
-          }:
-            enabled ++ [all.pcov];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      flake-utils,
+      pre-commit-hooks,
+      gitignore,
+      nix-phps,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        buildEnv =
+          php:
+          php.buildEnv {
+            extraConfig = "memory_limit = 2G";
+            extensions =
+              {
+                enabled,
+                all,
+              }:
+              enabled ++ [ all.pcov ];
+          };
+        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+        src = gitignore.lib.gitignoreSource ./.;
+
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          inherit src;
+          hooks = {
+            actionlint.enable = true;
+            #alejandra.enable = true;
+            #alejandra.excludes = ["\/vendor\/"];
+            shellcheck.enable = true;
+          };
         };
-      pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-      src = gitignore.lib.gitignoreSource ./.;
 
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        inherit src;
-        hooks = {
-          actionlint.enable = true;
-          alejandra.enable = true;
-          alejandra.excludes = ["\/vendor\/"];
-          shellcheck.enable = true;
+        makeShell =
+          { php }:
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              actionlint
+              alejandra
+              mdl
+              php
+              #(buildEnv php)
+              php.packages.composer
+              pre-commit
+            ];
+            shellHook = ''
+              ${pre-commit-check.shellHook}
+              export PATH="$PWD/vendor/bin:$PATH"
+            '';
+          };
+      in
+      rec {
+        checks = {
+          inherit pre-commit-check;
         };
-      };
 
-      makeShell = {php}:
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            actionlint
-            alejandra
-            mdl
-            php
-            #(buildEnv php)
-            php.packages.composer
-            pre-commit
-          ];
-          shellHook = ''
-            ${pre-commit-check.shellHook}
-            export PATH="$PWD/vendor/bin:$PATH"
-          '';
+        devShells = rec {
+          php74 = makeShell { php = nix-phps.packages.x86_64-linux.php74; };
+          php81 = makeShell { php = pkgs.php81; };
+          php82 = makeShell { php = pkgs.php82; };
+          php83 = makeShell { php = pkgs.php83; };
+          php84 = makeShell { php = pkgs-unstable.php84; };
+          default = php81;
         };
-    in rec {
-      checks = {
-        inherit pre-commit-check;
-      };
 
-      devShells = rec {
-        php81 = makeShell {php = pkgs.php81;};
-        php82 = makeShell {php = pkgs.php82;};
-        php83 = makeShell {php = pkgs.php83;};
-        php84 = makeShell {php = pkgs-unstable.php84;};
-        default = php81;
-      };
-
-      formatter = pkgs.alejandra;
-    });
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
 }
